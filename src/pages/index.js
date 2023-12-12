60273e7c8e31dcbbe5bc1a9ad241db8833ff0d55
benja <benjamin.astudillo.loyer@gmail.com>
2023-12-12 06:46:26 -0300

    feat finnal changes draft 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import '@fontsource/rubik'
 import styled from "styled-components"
 import './styles.css'
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticImage, GatsbyImage } from "gatsby-plugin-image"
 import scrollTo from 'gatsby-plugin-smoothscroll';
 import { Helmet } from "react-helmet"
 import { useEffect, useState } from "react"
@@ -56,7 +56,6 @@ const IndexPage = () => {
     };
   }, []);
 
-
   return (
     <main>
 
@@ -65,78 +64,98 @@ const IndexPage = () => {
           <Logo className="logo-head"></Logo>
           <h1>Diseño Web <span>Gratis</span> para bares y restaurantes</h1>
           <div className="whats_the_catch">
+
             <div className="catch_CTA">
               <h2>Sin Costo? =&gt;</h2>
               <div className="scarecity">
-                <p>Tenemos cupos limitados para esta oferta, y solo trabajamos con 1 cliente a la vez, asi que si te interesa, contactanos pronto para reservar tu cupo</p>
+                <p className="adjustable-f-size">Tenemos cupos limitados para esta oferta, y solo trabajamos con 1 cliente a la vez, asi que si te interesa, contactanos pronto para reservar tu cupo</p>
                 <button onClick={() => scrollTo("#calendly")} className="redeem_btn">me interesa</button>
               </div>
             </div>
+
             <div className="catch_list">
               <h3>Si, no hay costo para esta oferta mientras estes dispuesto ha</h3>
               <ul className='catch_list_wrapper'>
-                <li className="catch_list_item">Darnos un review y testimonio honestos de tu experiencia</li>
-                <li className="catch_list_item">Contestar un formulario, para ayudarnos a mejorar nuestro servicio</li>
-                <li className="catch_list_item">Referirnos a alguien que puede beneficiar de nuestros servicios, en caso de lo veas pertinente</li>
+                <li className="catch_list_item adjustable-f-size">Darnos un review y testimonio honestos de tu experiencia</li>
+                <li className="catch_list_item adjustable-f-size">Contestar un formulario, para ayudarnos a mejorar nuestro servicio</li>
+                <li className="catch_list_item adjustable-f-size">Referirnos a alguien que puede beneficiar de nuestros servicios, en caso de lo veas pertinente</li>
               </ul>
             </div>
           </div>
         </section>
+
+
         <div className="line-break" />
-        <section className='solution de-que-es'>
-          <h1>De que se trata?</h1>
-          <p>El tema aqui es un plan para aumentar la cantidad y mejorar la conversión de visitas por medios web. <br/>
-          Que significa esto? simplemente que mas gente se interese en ir a comer/tomar despues de ver algo por internet de tu local. Esto lo lograremos a traves de multiples medios que estan listados abajo por si te interesa averiguar mas de ellos. <br/>
-          Ademas de esto, tambien tenemos un enfoque en aumentar la frecuencia de tus clientes, para que puedas tener una fuente de ingresos mas recurrentes.
-          </p>
+        <section>
+          <div className='solution de-que-es'>
+            <h1>De que se trata?</h1>
+            <p className="adjustable-f-size">El tema aqui es un plan para aumentar la cantidad y mejorar la conversión de visitas por medios web. <br/>
+            Que significa esto? simplemente que mas gente se interese en ir a comer/tomar despues de ver algo por internet de tu local. Esto lo lograremos a traves de multiples medios que estan listados abajo por si te interesa averiguar mas de ellos. <br/>
+            Ademas de esto, tambien tenemos un enfoque en aumentar la frecuencia de tus clientes, para que puedas tener una fuente de ingresos mas recurrente.
+            </p>
+          </div>
         </section>
+
+
         <section className="solution">
           <div className="solution_webdev">
             <div className="title-wrapper">
               <h2>Sitios Web programados en <a target='_blank' href='https://gatsbyjs.com'>Gatsby</a></h2>
               <StaticImage src="../images/icon.png" className="gatsby-logo" alt="gatsby logo"></StaticImage>
             </div>
+
             <div className="paragraph-wrapper solution-block">
-              <StaticImage src="../images/ssg-host-flow.png" className="code-img slide-L-target solution-img" alt="code editor, with gatsby code"></StaticImage>
-              <p className="slide-R-target">Sitios web extremadamente rapidos, gracias a un modelo de generación estatica</p>
+              <StaticImage src="../images/ssg-host-flow.jpg" className="code-img slide-L-target solution-img" alt="code editor, with gatsby code"></StaticImage>
+              <p className="slide-R-target adjustable-f-size">Sitios web extremadamente rapidos, gracias a un modelo de generación estatica</p>
             </div>
+
             <div className='insights-wrapper solution-block'>
-              <p className="slide-L-target">Optimización, accesibilidad en multiples dipositivos y velocidad que verificamos con <a target="_blac" href='https://pagespeed.web.dev'>google page speed insights</a></p>
+              <p className="slide-L-target adjustable-f-size">Optimización, accesibilidad en multiples dipositivos y velocidad que verificamos con <a target="_blac" href='https://pagespeed.web.dev'>google page speed insights</a></p>
               <StaticImage src="../images/google-pagespeed-insights-logo" className="insights-logo slide-R-target solution-img" alt="page speed insights example"></StaticImage>
             </div>
           </div>
+
           <div className="mail-prismic-wrapper fade-in">
             <div className="solution_prismic solution-block">
               <StaticImage src="../images/prismic-logo.png" className="prismic-img slide-L-target solution-img" alt="logo prismic"></StaticImage>
-              <p className="slide-R-target">Podras editar el contenido del sitio a gusto, sin necesidad de escribir codigo gracias a que integramos de <a href="https://prismic.io" target="_blank">prismic.io</a></p>
+              <p className="slide-R-target adjustable-f-size">Podras editar el contenido del sitio a gusto, sin necesidad de escribir codigo gracias a que integramos de <a href="https://prismic.io" target="_blank">prismic.io</a></p>
             </div>
+
             <div className="solution_email solution-block">
-              <p className="slide-L-target">Mejoraremos tu retención de clientes, informadolos de tus offertas y descuentos con correos automatizados</p>
-              <StaticImage src="../images/mailerlite-logo" className="email-img slide-R-target solution-img" alt="logo email"></StaticImage>
+              <p className="slide-L-target adjustable-f-size">Mejoraremos tu retención de clientes, informadolos de tus offertas y descuentos con correos automatizados</p>
+              <StaticImage src="../images/mailerlite-logo.jpg" className="email-img slide-R-target solution-img" alt="logo email"></StaticImage>
             </div>
           </div>
+
           <div className="solution_GMB solution-block">
             <StaticImage src="../images/google-my-business-logo" className='gmb-img slide-L-target solution-img' ></StaticImage>
-            <p className="slide-R-target">Incrementamos busquedas locales con optimización de GMB (Google my business)</p>
+            <p className="slide-R-target adjustable-f-size">Incrementamos busquedas locales con optimización de GMB (Google my business)</p>
           </div>
+
         </section>
         <div className="line-break" />
         <section className="audience">
           <h3>Para quien es este servicio?</h3>
-          <p>Nuestro servicio es ideal para bares y restaurantes en concepción y san pedro de la paz</p>
-          <div className="map-imbed" dangerouslySetInnerHTML={{ __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d25548.267636835168!2d-73.07018214282225!3d-36.829698205105316!3m2!1i1024!2i768!4f13.1!2m1!1sbares!5e0!3m2!1ses-419!2scl!4v1700429366341!5m2!1ses-419!2scl" width="100%" height="560" style="border: 0; border-radius: 14px; box-shadow: rgba(2, 1, 0, 0.14) 0px 1px 41px 2px; margin-bottom: 10px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' }} />
+          <p className="adjustable-f-size">Nuestro servicio es ideal para bares y restaurantes en concepción y san pedro de la paz</p>
+          <div className="map-imbed" dangerouslySetInnerHTML={{ __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d25548.267636835168!2d-73.07018214282225!3d-36.829698205105316!3m2!1i1024!2i768!4f13.1!2m1!1sbares!5e0!3m2!1ses-419!2scl!4v1700429366341!5m2!1ses-419!2scl" width="100%" height="100%" style="border: 0; border-radius: 14px; box-shadow: rgba(2, 1, 0, 0.14) 0px 1px 41px 2px; margin-bottom: 10px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' }} />
         </section>
+
+
         <section className="identity">
-          <StaticImage src="../images/carnet-pic.jpeg" className="benjamin-img" ></StaticImage>
-          <p>Yo soy Benjamin Astudillo, in joven empresario y desarrollador web que lleva mas de 2 años estudiando y practicando para mejorar mis servicios</p>
+          <StaticImage src="../images/carnet-pic.jpg" className="benjamin-img slide-L-target" ></StaticImage>
+          <p className="adjustable-f-size slide-R-target">Yo soy Benjamin Astudillo, un joven desarrollador web y empresario que lleva mas de 2 años estudiando y practicando para mejorar mis servicios</p>
         </section>
+
+
         <section id="calendly">
-          <h2>Coordina una videollamada sin atados, para discutir si te interesa nuestro servicio</h2>
-          <Helmet>
-            <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
-          </Helmet>
-          <div id="calendly-imbed">
-            <div class="calendly-inline-widget" data-url="https://calendly.com/holdtml-benjamin/reunion-inicial?primary_color=df7b1e" style={{ minWidth: '320px', height: '700px', width: '100%', margin: ' 0px 0px 120px 0px', padding: '0px' }}></div>
+          <h2 className="slide-R-target">Coordina una videollamada sin atados, para discutir si te interesa nuestro servicio</h2>
+          <div class="calendly-responsive-wrapper">
+            <Helmet>
+              <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+            </Helmet>
+            <div id="calendly-imbed">
+              <div class="calendly-inline-widget" data-url="https://calendly.com/holdtml-benjamin/reunion-inicial?primary_color=df7b1e" style={{ minWidth: '320px', height: '82dvh', width: '100%', margin: ' 0px 0px 120px 0px', padding: '0px' }}></div>
+            </div>
           </div>
         </section>
       </div>
